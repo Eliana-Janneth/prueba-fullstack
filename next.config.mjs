01f9f2153049b7913c4d00aa342130898f87d4bc
Eliana Puerta <eliana.puerta@udea.edu.co>
2025-03-20 19:59:03 -0500

    fix: update Next.js configuration for Auth0

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
     AUTH0_ISSUER: process.env.AUTH0_ISSUER,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID_ADMIN: process.env.AUTH0_CLIENT_ID_ADMIN,
+    AUTH0_CLIENT_SECRET_ADMIN: process.env.AUTH0_CLIENT_SECRET_ADMIN,
   },
 };
 
